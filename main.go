9724f2694413710377d6664f7142f6db1f9ea622
Vishnu Nair <vish@vnair.me>
2019-01-26 12:24:45 -0500

    Wrote function to get all list URLs.

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+)
+
+var piholeListRoot = "/etc/pihole"
+var ipAddrRegex = regexp.MustCompile(`\b(?:\d{1,3}\.){3}\d{1,3}\b`)
+
+type ListResult struct {
+	LineNumber   int
+	ListFileName string
+	ListURL      string
+}
+
+type TotalResult struct {
+	QueryURL     string
+	AllBLMatches []ListResult
+}
+
+func getAllListURLs() []string {
+	list_urls_filename := filepath.Join(piholeListRoot, "adlists.list")
+	full_url_list, err := os.Open(list_urls_filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer full_url_list.Close()
+
+	url_list := make([]string, 25)
+	file_scanner := bufio.NewScanner(full_url_list)
+	for file_scanner.Scan() {
+		url_list = append(url_list, file_scanner.Text())
+	}
+
+	if err := file_scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return url_list
+}
+
+func main() {
+
+}
